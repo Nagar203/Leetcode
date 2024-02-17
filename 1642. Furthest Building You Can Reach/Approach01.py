0b3280396ad8fd2c298118d257a4b2373ab436ba
cnagar <cmdhakad93@gmail.com>
2024-02-17 22:55:52 +0530

    17 Feb, 2024

---
+++ 1642. Furthest Building You Can Reach/Approach01.py
@@ -0,0 +1,21 @@
+from typing import *
+import heapq
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        
+        n = len(heights)
+        pq = []
+
+        for i in range(1, n):
+            diff = heights[i] - heights[i - 1]
+            if diff >= 0:
+                heapq.heappush(pq, diff)
+
+                if len(pq) > ladders:
+                    bricks -= heapq.heappop(pq)
+
+                if bricks < 0:
+                    return i - 1
+
+        return n - 1
