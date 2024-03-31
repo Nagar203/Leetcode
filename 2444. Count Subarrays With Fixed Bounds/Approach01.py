1304fafd9142a8889cfd15d42e2feaae0a6800c6
cnagar <cmdhakad93@gmail.com>
2024-03-31 19:36:10 +0530

    31 Mar, 2024

---
+++ 2444. Count Subarrays With Fixed Bounds/Approach01.py
@@ -0,0 +1,25 @@
+from typing import *
+
+class Solution:
+    def countSubarrays(self, nums, minK, maxK):
+        ans = 0
+        
+        minPosition = -1
+        maxPosition = -1
+        leftBound   = -1
+        
+        for i in range(len(nums)):
+            
+            if nums[i] < minK or nums[i] > maxK:
+                leftBound = i
+            
+            if nums[i] == minK:
+                minPosition = i
+            if nums[i] == maxK:
+                maxPosition = i
+            
+            count = min(maxPosition, minPosition) - leftBound
+            
+            ans += 0 if count <= 0 else count
+        
+        return ans
