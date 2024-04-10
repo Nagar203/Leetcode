efadbf5ac71ffdc3a0ddf01398424f4262b85b23
cnagar <cmdhakad93@gmail.com>
2024-04-10 21:58:09 +0530

    09 April, 2024

---
+++ 2073. Time Needed to Buy Tickets/Approach01.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int timeRequiredToBuy(vector<int>& tickets, int k) {
+    int result = 0;
+
+    for (int i = 0; i < tickets.size(); ++i)
+      if (i <= k)
+        result += min(tickets[i], tickets[k]);
+      else
+        result += min(tickets[i], tickets[k] - 1);
+
+    return result;
+  }
+};
