96bc9b22981b1fba17edd9e2398b05efde3abafc
cnagar <cmdhakad93@gmail.com>
2024-05-20 21:01:46 +0530

    20 May, 2024

---
+++ 1863. Sum of All Subset XOR Totals/Approach01.py
@@ -0,0 +1,13 @@
+from typing import *
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def solve(i: int, xors: int) -> int:
+            if i == len(nums):
+                return xors
+
+            x = solve(i + 1, xors)
+            y = solve(i + 1, nums[i] ^ xors)
+            return x + y
+
+        return solve(0, 0)
