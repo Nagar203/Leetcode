43177fe7f4d21128a62597b8fc695f499204c607
cnagar <cmdhakad93@gmail.com>
2024-06-04 23:25:12 +0530

    04 June, 2024

---
+++ 409. Longest Palindrome/Approach01.py
@@ -0,0 +1,13 @@
+from typing import *
+import collections
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        result = 0
+        count = collections.Counter(s)
+
+        for c in count.values():
+            result += c if c % 2 == 0 else c - 1
+
+        hasOddCount = any(c % 2 == 1 for c in count.values())
+        return result + hasOddCount
