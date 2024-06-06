a60fb12926a02453d3c766003f7d1a646a26326d
cnagar <cmdhakad93@gmail.com>
2024-06-06 23:15:59 +0530

    05 June, 2024

---
+++ 1002. Find Common Characters/Approach01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  vector<string> commonChars(vector<string>& words) {
+    vector<string> result;
+    vector<int> commonCount(26, INT_MAX);
+
+    for (const string& word : words) {
+      vector<int> count(26);
+      for (const char c : word)
+        ++count[c - 'a'];
+      for (int i = 0; i < 26; ++i)
+        commonCount[i] = min(commonCount[i], count[i]);
+    }
+
+    for (char c = 'a'; c <= 'z'; ++c)
+      for (int i = 0; i < commonCount[c - 'a']; ++i)
+        result.push_back(string(1, c));
+
+    return result;
+  }
+};
---
+++ 1002. Find Common Characters/Approach01.py
@@ -0,0 +1,21 @@
+from typing import *
+
+
+from typing import List
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        result = []
+        commonCount = [float('inf')] * 26
+
+        for word in words:
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            for i in range(26):
+                commonCount[i] = min(commonCount[i], count[i])
+
+        for i in range(26):
+            result.extend([chr(i + ord('a'))] * commonCount[i])
+
+        return result
