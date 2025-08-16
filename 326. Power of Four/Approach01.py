b3724efc8328bbe5c3d54c0166371205da970bb2
cnagar <cmdhakad93@gmail.com>
2025-08-16 22:58:00 +0530

    updates

--- 326. Power of Four/Approach01.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPowerOfFour(self, number: int) -> bool:
-        while number:
-            if number == 1:
-                return True
-            if number % 4 != 0:
-                return False
-            number //= 4
-        return False
